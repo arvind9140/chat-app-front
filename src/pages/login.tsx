d4f45630c7e1323524e1b3de2571c55ee8dfd45e
devashish gupta <89517503+Itsdevashishgupta@users.noreply.github.com>
2024-01-16 19:19:46 +0530

    Update login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -67,7 +67,7 @@ const Login = () => {
             ></SidebarItem>
           </Link>
 
-          <Link to="https:/colonelz-frontend.vercel.app/inventory">
+          <Link to="https://colonelz-frontend.vercel.app/inventory">
             <SidebarItem
               icon={<Warehouse />}
               text="Inventory"
@@ -75,7 +75,7 @@ const Login = () => {
             ></SidebarItem>
           </Link>
 
-          <Link to="https:/colonelz-frontend.vercel.app/mom">
+          <Link to="https://colonelz-frontend.vercel.app/mom">
             <SidebarItem
               icon={<Timer />}
               text="MOM"
@@ -83,7 +83,7 @@ const Login = () => {
             ></SidebarItem>
           </Link>
 
-          <Link to="https:/colonelz-frontend.vercel.app/lead">
+          <Link to="https://colonelz-frontend.vercel.app/lead">
             <SidebarItem
               icon={<Users />}
               text="Lead Management"
