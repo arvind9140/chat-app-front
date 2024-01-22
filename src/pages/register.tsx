234f72354b68fc6604db392bb415683686154b87
Arvind Maurya <ar789maurya@gmail.com>
2024-01-22 12:49:21 +0530

    update

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -165,7 +165,7 @@ const Register = () => {
      
       </div>
       <div className="flex justify-center items-center flex-col h-screen w-screen ">
-        <h1 className="text-3xl font-bold">Chat App</h1>
+       
         <div className="max-w-5xl w-1/2 p-8 flex justify-center items-center gap-5  flex-col bg-white text-dark shadow-md rounded-2xl my-16 border-secondary border-[1px]">
           <h1 className="inline-flex items-center text-2xl mb-4 flex-col ">
             {/* Lock icon */}
